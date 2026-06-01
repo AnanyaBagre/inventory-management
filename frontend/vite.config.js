42939502db5b72c93a27f22d4810b68333d620a8
Ananya Bagre <129136204+AnanyaBagre@users.noreply.github.com>
2026-06-01 23:40:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      'inventory-management-mskb.onrender.com',
-      'inventory-management-1-nn7w.onrender.com'
-    ]
-  }
-})
